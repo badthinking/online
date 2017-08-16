34715fd3b7b589ec1d8d37c48875381e8b4dde22
Pranav Kant <pranavk@collabora.co.uk>
2017-08-16 16:50:58 +0530

    lokdialog: Add dialog related callbacks
    Change-Id: I5dc54c4bdbaf066b3b089a00fbc89b943e73caae

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -1243,6 +1243,12 @@ void ChildSession::loKitCallback(const int type, const std::string& payload)
     case LOK_CALLBACK_CELL_ADDRESS:
         sendTextFrame("celladdress: " + payload);
         break;
+    case LOK_CALLBACK_DIALOG:
+        sendTextFrame("dialog: " + payload);
+        break;
+    case LOK_CALLBACK_DIALOG_CHILD:
+        sendTextFrame("dialogchild: " + payload);
+        break;
     default:
         LOG_ERR("Unknown callback event (" << type << "): " << payload);
     }
